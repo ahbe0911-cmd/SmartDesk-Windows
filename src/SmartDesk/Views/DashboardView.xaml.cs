685047fcf75c89e70046bd197660a7a28cda307c
ahbe0911-cmd <ahbe0911@gmail.com>
2026-09-18 16:37:25 +0330

    fix: bind right quick-access cards to link collection

--- src/SmartDesk/Views/DashboardView.xaml.cs
+++ src/SmartDesk/Views/DashboardView.xaml.cs
@@ -26,6 +26,7 @@ public partial class DashboardView : UserControl
     {
         InitializeComponent();
         DashboardQuickLinks.ItemsSource = _links;
+        SidebarQuickLinks.ItemsSource = _links;
         _clockTimer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
         _clockTimer.Tick += (_, _) => UpdateClock();
         Loaded += (_, _) => { UpdateClock(); RenderCalendar(); _clockTimer.Start(); };
